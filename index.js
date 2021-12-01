719c3880cb43ef78a6fb318939e19b5e16b6729c
BokovRoman <r.bokov1991@gmail.com>
2021-12-01 20:12:55 +0200

    revue code

--- index.js
+++ index.js
@@ -1,72 +1,36 @@
-const { Command } = require('commander');
-
-const {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-} = require("./contacts");
+const { Command } = require("commander");
+const contactsOperation = require("./contacts");
 
 const program = new Command();
-
 program
-  .option('-a, --action <type>', 'choose action')
-  .option('-i, --id <type>', 'user id')
-  .option('-n, --name <type>', 'user name')
-  .option('-e, --email <type>', 'user email')
-  .option('-p, --phone <type>', 'user phone');
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
 program.parse(process.argv);
 
 const argv = program.opts();
 
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case 'list':
-      listContacts();
-      break;
-
-    case 'get':
-      getContactById(id);
-      break;
-
-    case 'add':
-      addContact(name, email, phone);
-      break;
-
-    case 'remove':
-      removeContact(id);
-      break;
-
-    default:
-      console.warn('\x1B[31m Unknown action type!');
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  try {
+    switch (action) {
+      case "list":
+        return await contactsOperation.listContacts();
+      case "get":
+        return await contactsOperation.getContactById(id);
+      case "add":
+        return await contactsOperation.addContact(name, email, phone);
+      case "remove":
+        return await contactsOperation.removeContactById(id);
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  } catch (error) {
+    throw error;
   }
-}
+};
+
 
 invokeAction(argv);
-
-// fs.readFile("db/contacts.json", "utf-8", (error, data) => {
-//     if (error) {
-//         throw new Error("Cannot read the File!")
-//     }
-//     console.log(error);
-//     console.log(data);
-// });
-
-// (async() => {
-//     const data = await fs.readFile("db/contacts.json", "utf-8");
-//     console.log(data);
-// })()
-
-
-// const readFile = async (filePath) => {
-//     try {
-//         const data = await fs.readFile(filePath, "utf-8");
-//         console.log(data);
-//     }
-//     catch(error) {
-//         console.log(error.message)
-//     }
-// };
-
-// readFile("db/contacts1.json");
